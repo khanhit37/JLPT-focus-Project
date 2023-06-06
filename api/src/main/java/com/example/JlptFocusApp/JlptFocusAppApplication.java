888fa8a48610742b618ba9f18a2af58c3290e653
Nguyen Duc Khanh <khanhnd3@rikkeisoft.com>
2023-06-06 16:15:35 +0900

    Upload Api Project

---
+++ api/src/main/java/com/example/JlptFocusApp/JlptFocusAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.JlptFocusApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlptFocusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlptFocusAppApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/example/JlptFocusApp/JlptFocusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JlptFocusApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlptFocusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
